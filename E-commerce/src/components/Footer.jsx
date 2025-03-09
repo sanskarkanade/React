e7a97e107f5084e7d68cbe8ebb1533f54b95e976
sanskarkanade <kanadesanskar79@gmail.com>
2025-03-09 11:57:13 +0530

    Footer links added

--- E-commerce/src/components/Footer.jsx
+++ E-commerce/src/components/Footer.jsx
@@ -9,16 +9,16 @@ const Footer = () => {
           <p>© 2025 All rights reserved. | Terms of Service | Privacy Policy | Made by Sanskar Kanade</p>
           <div className="flex gap-4 items-center">
             <p>Connect me on </p>
-            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/sanskar-kanade-585805322?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">
               <FaLinkedin className="text-xl hover:text-blue-600 transition" />
             </a>
-            <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/sanskarkanade" target="_blank" rel="noopener noreferrer">
               <FaGithub className="text-xl hover:text-gray-500 transition" />
             </a>
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/sanskar_1776?igsh=MWV5azV2Y3Zuc2tmeQ==" target="_blank" rel="noopener noreferrer">
               <FaInstagram className="text-xl hover:text-pink-500 transition" />
             </a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://x.com/Sanskarkanade17?t=i5NsBVQpYgrWcmG0nO9SlQ&s=09" target="_blank" rel="noopener noreferrer">
               <FaTwitter className="text-xl hover:text-blue-400 transition" />
             </a>
           </div>
